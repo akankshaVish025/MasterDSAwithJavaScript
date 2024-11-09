caaba1fa191b8ebe311769948480af335d0f246d
akanksha-V-2599 <vish.akanksha099@gmail.com>
2024-11-09 18:52:17 +1300

    Added new patterns

---
+++ Patterns/1_Triangle_pattern.js
@@ -0,0 +1,21 @@
+// A triangle pattern:
+
+function printPattern(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log('* '.repeat(i)); // * with or without space is okay
+    }
+}
+
+printPattern(5);
+
+/**
+Output:
+* 
+* * 
+* * *
+* * * *
+* * * * *
+*/
+
+// Time Complexity: O(n^2)
+// Explanation: The outer loop runs n times, and the inner operation (printing stars) runs proportionally to the current line number.
